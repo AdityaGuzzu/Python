95d30149aba49ba0756cabf387248b8f7a4d8b5f
Gujju Aditya <guzzuaditya@gmail.com>
2022-08-24 23:12:56 +0530

    Done with for loop

---
+++ loops/for loop in lists.py
@@ -0,0 +1,5 @@
+#We will use for loop in lists.
+marks = [10,20,30,24,45]
+
+for element in marks:
+    print(element)
---
+++ loops/range.py
@@ -0,0 +1,9 @@
+#use range based for loop with the 'range' keyword
+
+for i in range(10): #takes 0 as the initial index
+    print(i)
+
+print("Now using two indices between 20 and 25: ")
+
+for i in range(20,25):  #excludes the upper limit while including the lower limit
+    print(i)
